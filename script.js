915f88e2792f7426e95a21807e909e3cfa548915
own <ffdffrttgf@outlook.com>
2024-09-20 11:31:15 -0600

    added rebirths
    skimmed down css file and added rebirths also fixed grid and made mine gold button a top item

--- script.js
+++ script.js
@@ -3,27 +3,33 @@ var gameData = {
     goldPerClick: 1,
     goldUpgradePerClickCost: 10,
     autoMiners: 1,
-    autoMinersPurchaseCost: 20
+    autoMinersPurchaseCost: 20,
+    rebirth: 1,
+    rebirthCost: 5000,
+    rebirthDisplay: 0
 }
 const perClickUpgrade = document.getElementById("perClickUpgrade")
 const goldMined = document.getElementById("goldMined")
 const buyAutoMinersButton = document.getElementById("buyAutoMinersButton")
+const buyRebirthButton = document.getElementById("buyRebirthButton")
 var savegame = JSON.parse(localStorage.getItem("goldMinerSave"))
 if (savegame !== null) {
     gameData = savegame
     perClickUpgrade.innerHTML = " Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: "+ gameData.goldUpgradePerClickCost + " Gold"
+    buyAutoMinersButton.innerHTML = " Buy Auto Miner (Current Auto Miners " + gameData.autoMiners + ") Cost: "+ gameData.autoMinersPurchaseCost + " Gold"
+    buyRebirthButton.innerHTML = " Rebirth (Current Rebirths " + gameData.rebirthDisplay + ") Cost: "+ gameData.rebirthCost + " Gold"
     goldMined.innerHTML = gameData.gold + " Gold Mined"
 }
 function mineGold()
 {
-    gameData.gold += gameData.goldPerClick
+    gameData.gold += gameData.goldPerClick * gameData.rebirth 
     goldMined.innerHTML = gameData.gold + " Gold Mined"
 }
 function autoMinersMine()
 {
         let randomDelayAutoMiner = Math.floor(Math.random() * (400 - 100 + 1)) + 100;
         setTimeout(function() {
-            gameData.gold += gameData.goldPerClick * gameData.autoMiners
+            gameData.gold += gameData.goldPerClick * gameData.rebirth * gameData.autoMiners
        }, randomDelayAutoMiner);
        goldMined.innerHTML = gameData.gold + " Gold Mined"
 }
@@ -46,6 +52,24 @@ function buyAutoMiners()
         gameData.autoMiners++
         gameData.autoMinersPurchaseCost *= 2
         buyAutoMinersButton.innerHTML = " Buy Auto Miner (Current Auto Miners " + gameData.autoMiners + ") Cost: "+ gameData.autoMinersPurchaseCost + " Gold"
+        goldMined.innerHTML = gameData.gold + " Gold Mined"
+    }
+}
+function buyRebirth() 
+{
+    if (gameData.gold >= gameData.rebirthCost)
+    {
+        gameData.gold = 0
+        gameData.autoMiners = 1
+        gameData.goldPerClick = 1
+        gameData.goldUpgradePerClickCost = 10
+        gameData.autoMinersPurchaseCost = 20
+        gameData.rebirthCost *= 3
+        gameData.rebirth++
+        gameData.rebirthDisplay++
+        perClickUpgrade.innerHTML = " Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: "+ gameData.goldUpgradePerClickCost + " Gold"
+        buyAutoMinersButton.innerHTML = " Buy Auto Miner (Current Auto Miners " + gameData.autoMiners + ") Cost: "+ gameData.autoMinersPurchaseCost + " Gold"
+        buyRebirthButton.innerHTML = " Rebirth (Current Rebirths " + gameData.rebirthDisplay + ") Cost: "+ gameData.rebirthCost + " Gold"
         goldMined.innerHTML = gameData.gold + " Gold Mined"
     }
 }
